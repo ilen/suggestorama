a8581d2e9beff4925de0d92e2827d17a59fe7489
ilen <ilen@ilen.info>
2009-06-13 19:13:36 -0400

    voting works

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,11 @@
 class VotesController < ApplicationController
+  
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @vote = @topic.votes.create!
+    flash[:notice] = "you voted for #{@vote.topic.title}"
+    redirect_to(topics_path)
+  end
+
+
 end
